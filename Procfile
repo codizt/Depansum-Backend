1d05bc245eda982fc4e2eddbba1940d1fe5f5fd1
Codizt <thecodizt@gmail.com>
2021-06-25 17:04:07 +0530

    🚀 Added deployment files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn depansum.wsgi
--- depansum/settings.py
+++ depansum/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-n^%%bi&0d8r7+$$4$r0eg*2x$_b$+uj9gqu968f1&e^-)8d-a!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,3 +124,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
